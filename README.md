5a7aee09df84c146430e42746a9dd896c179df37
Guozheng Ge <guozheng.ge@gmail.com>
2016-07-09 00:42:22 -0700

    switch to use Java 8 lamda

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # hello_spark
-The classic word count example based on http://www.robertomarchetto.com Spark tutorial
+The classic word count example before Java 8 and after Java 8+
 
 ## Environment setup
 I use [cdh5-pseudo-distributed](https://hub.docker.com/r/chalimartines/cdh5-pseudo-distributed/) docker image, it includes all essential components such as HDFS, YARN, Oozie, Hue, Spark on YARN, etc. Visit the image's [docker hub page](https://hub.docker.com/r/chalimartines/cdh5-pseudo-distributed/) for instructions to set it up.
 
 ## Deploy and run the example
-   * Set up a Docker volume `/tmp/docker` that maps to `~/tmp/docker` directory in local dev box. 
-   * Build the jar, copy the jar, sample input file `src/main/resources/loremipsum.txt`, the script `src/main/scripts/hellospark` into the shared directory.
+   * Set up a Docker volume `/tmp/docker` that maps to `~/tmp/docker` directory in local dev box.
+   * Clone the repo, run `mvn package` and build the jar file `target/hellospark-1.0-SNAPSHOT.jar`.
+   * Copy the jar, sample input file `src/main/resources/loremipsum.txt`, the script `src/main/scripts/hellospark` into the shared directory with Docker container.
+   * Log into Docker vm.
    * On HDFS, create the directory `/tmp/spark-out`, copy `loremipsum.txt` there (see put command in [file system shell manual](https://hadoop.apache.org/docs/r1.0.4/file_system_shell.html#put)). Make sure `/tmp/spark-out` is writable.
    * Run the script to submit the Spark job and wait for it to finish. Then you should see `/tmp/spark-out/wordcount-out/_SUCCESS` and `/tmp/spark-out/wordcount-out/part-00000`
 
--- src/main/java/com/hello/spark/WordCount.java
+++ src/main/java/com/hello/spark/WordCount.java
@@ -13,7 +13,7 @@ import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 
 /**
- * Sample Spark word count from: http://www.robertomarchetto.com/spark_java_maven_example
+ * Spark word count example in both before Java 8 and Java 8+
  *
  */
 public class WordCount 
@@ -34,26 +34,36 @@ public class WordCount
         		.setMaster("local");
         JavaSparkContext ctx = new JavaSparkContext(conf);
         JavaRDD<String> file = ctx.textFile(path);
-        JavaRDD<String> words = file.flatMap(WORDS_EXTRACTOR);
-        JavaPairRDD<String, Integer> pairs = words.mapToPair(WORDS_MAPPER);
-        JavaPairRDD<String, Integer> counter = pairs.reduceByKey(WORDS_REDUCER);
+        
+        // before Java 8
+//        JavaRDD<String> words = file.flatMap(WORDS_EXTRACTOR);
+//        JavaPairRDD<String, Integer> pairs = words.mapToPair(WORDS_MAPPER);
+//        JavaPairRDD<String, Integer> counter = pairs.reduceByKey(WORDS_REDUCER);
+        
+        // Java 8 and after
+        JavaRDD<String> words = file.flatMap(line -> Arrays.asList(line.split(" ")));
+        JavaPairRDD<String, Integer> pairs = words.mapToPair(w -> new Tuple2<String, Integer>(w, 1));
+        JavaPairRDD<String, Integer> counter = pairs.reduceByKey((x, y) -> x + y);
         
         counter.saveAsTextFile(outputPath);
         System.out.println("output path: " + outputPath);
     }
     
+    // turn each line into words
     private static final FlatMapFunction<String, String> WORDS_EXTRACTOR = new FlatMapFunction<String, String>() {
     	public Iterable<String> call(String s) throws Exception {
     		return Arrays.asList(s.split(" "));
     	}
     };
     
+    // turn words into (word, 1) pairs
     private static final PairFunction<String, String, Integer> WORDS_MAPPER = new PairFunction<String, String, Integer>() {
     	public Tuple2<String, Integer> call(String s) throws Exception {
     		return new Tuple2<String, Integer>(s, 1);
     	}
     };
     
+    // group by words and calculate the counts
     private static final Function2<Integer, Integer, Integer> WORDS_REDUCER = new Function2<Integer, Integer, Integer>() {
     	public Integer call(Integer a, Integer b) throws Exception {
     		return a + b;
